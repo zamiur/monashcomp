13d0984d4e0c2d58654ace8c0a4dba905afb5363
Zamiur <zrtan1@protonmail.ch>
2020-07-25 15:43:37 +1000

    Update attempt1-main.cpp

--- WinterTraining2/PathsOnTree/attempt1-main.cpp
+++ WinterTraining2/PathsOnTree/attempt1-main.cpp
@@ -1,99 +1,105 @@
 #include<iostream>
-#include<stdio.h>
-#include<map>
-#include<string.h>
-#include<math.h>
+
 #include<algorithm>
-#include<vector>
+
+#include<cstring>
+#include<cmath>
 #include<queue>
-#define inf 0x3f3f3f3f
+#include<vector>
+#include<map>
+#include<set>
+#include<cstdio>
+#define ll long long# define mset(a, x) memset(a, x, sizeof(a))
 using namespace std;
-const int N = 4e5 + 10;
-int n, m, maxid, maxn;
-vector < int > G[N];
-int f[N];
-int d[N];
-int vis[N];
-int path[N];
-int book[N];
-int du[N];
-int ans, ct, nid;
-void dfs(int x) {
-  for (int i = 0; i < G[x].size(); i++) {
-    int u = G[x][i];
-    if (vis[u] == 1) continue;
-    vis[u] = 1;
-    if (d[u] < d[x] + 1) {
-      d[u] = d[x] + 1;
-      if (maxn < d[u]) {
-        maxid = u;
-        maxn = d[u];
-      }
-      path[u] = x;
-    }
-    dfs(u);
-  }
-  return;
+const double PI = acos(-1);
+const int inf = 0x3f3f3f3f;
+const double esp = 1e-6;
+const int maxn = 1e6 + 5;
+const int mod = 1e9 + 7;
+int dir[4][2] = {
+  0,
+  1,
+  1,
+  0,
+  0,
+  -1,
+  -1,
+  0
+};
+ll gcd(ll a, ll b) {
+  return b ? gcd(b, a % b) : a;
 }
-void dfss(int x, int num) {
-  if (ct < num) {
-    ct = num;
-    nid = x;
+ll lcm(ll a, ll b) {
+  return a / gcd(a, b) * b;
+}
+ll inv(ll b) {
+  if (b == 1) return 1;
+  return (mod - mod / b) * inv(mod % b) % mod;
+}
+ll fpow(ll n, ll k) {
+  ll r = 1;
+  for (; k; k >>= 1) {
+    if (k & 1) r = r * n % mod;
+    n = n * n % mod;
   }
-  for (int i = 0; i < G[x].size(); i++) {
-    int u = G[x][i];
-    if (book[u] == 1) continue;
-    book[u] = 1;
-    dfss(u, num + 1);
+  return r;
+}
+ll Fpow(ll n, ll k) {
+  ll r = 1;
+  for (; k; k >>= 1) {
+    if (k & 1) r = r * n;
+    n = n * n;
   }
-  return;
+  return r;
+}
+ll upd(ll x, ll v) {
+  x = x + v >= mod ? x + v - mod : x + v;
+  return x;
+}
+struct node {
+  int next, v, u, w;
+}
+p[maxn];
+int head[maxn], cnt, vis[maxn], n, k, maxx;
+void add(int u, int v) {
+  p[cnt].v = v;
+  p[cnt].next = head[u];
+  head[u] = cnt++;
+}
+void dfs(int now, int fa, int step) {
+  maxx++;
+  if (step == 0) return;
+  for (int i = head[now]; ~i; i = p[i].next) {
+    if (p[i].v == fa) continue;
+    dfs(p[i].v, now, step - 1);
+  }
 }
 int main() {
-  int cnt = 1, st, ed, idd;
-  scanf("%d", & n); {
-    m = n - 1;
-    int flag = 0;
-    for (int i = 1; i <= n; i++) G[i].clear();
-    for (int i = 1; i <= n; i++) book[i] = 0;
-    for (int i = 1; i <= m; i++) {
-      int ta, tb;
-      scanf("%d%d", & ta, & tb);
-      G[ta].push_back(tb);
-      G[tb].push_back(ta);
-    }
-    for (int i = 1; i <= n; i++) vis[i] = 0;
-    maxn = 0;
-    maxid = 0;
-    dfs(1);
-    int tem = maxid;
-    for (int i = 1; i <= n; i++) vis[i] = 0;
-    for (int i = 1; i <= n; i++) path[i] = -1;
-    for (int i = 1; i <= n; i++) d[i] = 0;
-    maxid = 0;
-    maxn = 0;
-    path[tem] = -1;
-    vis[tem] = 1;
-    dfs(tem);
-    st = maxid;
-    int idd;
-    for (int i = maxid; i != -1; i = path[i]) {
-      book[i] = 1;
-      ed = i;
-    }
-    for (int i = 1; i <= n; i++)
-      if (book[i] && i != st && i != ed) idd = i;
-    ct = 0, nid = idd;
-    ans = maxn;
-    for (int i = 1; i <= n; i++)
-      if (book[i] == 1) {
-        ct = 0;
-        dfss(i, 0);
-        if (ans < maxn + ct) {
-          ans = maxn + ct;
-          idd = nid;
-        }
-      }
-    printf("%d\n%d %d %d\n", ans, st, ed, idd);
+  int i, j, x, y;
+  scanf("%d%d", & n, & k);
+  for (i = 0; i <= n; i++) head[i] = -1;
+  for (i = 0; i < n - 1; i++) {
+    scanf("%d%d", & x, & y);
+    add(x, y);
+    add(y, x);
   }
-  return 0;
+  int ans = 0;
+  if (k % 2) {
+    for (i = 1; i <= n; i++) {
+      for (j = head[i]; ~j; j = p[j].next) {
+        maxx = 0;
+        int t = p[j].v;
+        dfs(t, i, k / 2);
+        dfs(i, t, k / 2);
+        ans = max(maxx, ans);
+      }
+    }
+  } else {
+    for (i = 1; i <= n; i++) {
+      maxx = 0;
+      dfs(i, 0, k / 2);
+      ans = max(maxx, ans);
+    }
+  }
+  printf("%d\n", n - ans);
 }
---
+++ WinterTraining2/ShortenDiameter/attempt1-main.cpp
@@ -0,0 +1,87 @@
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+#include<cmath>
+#include<queue>
+#include<vector>
+#include<map>
+#include<set>
+#include<cstdio>
+#define ll long long
+#define mset(a,x) memset(a,x,sizeof(a))
+using namespace std;
+const double PI=acos(-1);
+const int inf=0x3f3f3f3f;
+const double esp=1e-6;
+const int maxn=1e6+5;
+const int mod=1e9+7;
+int dir[4][2]={0,1,1,0,0,-1,-1,0};
+ll gcd(ll a,ll b){return b?gcd(b,a%b):a;}
+ll lcm(ll a,ll b){return a/gcd(a,b)*b;}
+ll inv(ll b){if(b==1)return 1; return (mod-mod/b)*inv(mod%b)%mod;}
+ll fpow(ll n,ll k){ll r=1;for(;k;k>>=1){if(k&1)r=r*n%mod;n=n*n%mod;}return r;}
+ll Fpow(ll n,ll k){ll r=1;for(;k;k>>=1){if(k&1)r=r*n;n=n*n;}return r;}
+ll upd(ll x,ll v){x=x+v>=mod?x+v-mod:x+v;return x;}
+struct node{
+	int next,v,u,w;
+}p[maxn];
+int head[maxn],cnt,vis[maxn],n,k,maxx;
+ 
+void add(int u,int v)
+{
+	p[cnt].v=v;
+	p[cnt].next=head[u];
+	head[u]=cnt++;
+}
+ 
+void dfs(int now,int fa,int step)
+{
+	maxx++;
+	if(step==0)
+	return ;
+	for(int i=head[now];~i;i=p[i].next)
+	{
+		if(p[i].v==fa)
+		continue;
+		dfs(p[i].v,now,step-1);
+	}
+}
+ 
+int main()
+{
+	int i,j,x,y;
+	scanf("%d%d",&n,&k);
+	for(i=0;i<=n;i++)
+	head[i]=-1;
+	for(i=0;i<n-1;i++)
+	{
+		scanf("%d%d",&x,&y);
+		add(x,y);
+		add(y,x);
+	}
+	int ans=0;
+	if(k%2)
+	{
+		for(i=1;i<=n;i++)
+		{
+			for(j=head[i];~j;j=p[j].next)
+			{
+				maxx=0;
+				int t=p[j].v;
+				dfs(t,i,k/2);
+				dfs(i,t,k/2);
+				ans=max(maxx,ans);
+			}
+		}
+	}
+	else
+	{
+		for(i=1;i<=n;i++)
+		{
+			maxx=0;
+			dfs(i,0,k/2);
+			ans=max(maxx,ans);
+		}
+	}
+	printf("%d\n",n-ans);
+}
